0c03570224ba2548f79fb865af29e57b9ece4cf1
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-25 01:28:55 +0700

    Update GameConfig.java

--- app/src/main/java/com/universal/performance/GameConfig.java
+++ app/src/main/java/com/universal/performance/GameConfig.java
@@ -4,58 +4,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GameConfig {
-    // 🎮 GAME 1: QQ飞车 (China)
     public static final String PKG_QQ_SPEED = "com.tencent.tmgp.speedmobile";
     public static final String NAME_QQ_SPEED = "QQ Speed / QQ飞车";
-    public static final String VER_QQ_SPEED = "1.58.0.50557";
-    
-    // 🎮 GAME 2: Speed Drifters (Global/Taiwan)
     public static final String PKG_SPEED_DRIFTERS = "com.garena.game.fctw";
     public static final String NAME_SPEED_DRIFTERS = "Speed Drifters";
-    public static final String VER_SPEED_DRIFTERS = "1.54.0.26207";
 
-    // Supported Game List
     public static final List<String> SUPPORTED_GAMES = Arrays.asList(
-        PKG_QQ_SPEED,
-        PKG_SPEED_DRIFTERS,
-        "com.tencent.tmgp.speedmobile",
-        "com.garena.game.fctw",
-        "com.miHoYo.GenshinImpact",
-        "com.tencent.tmgp.pubgmhd",
-        "com.mobile.legends",
-        "com.dts.freefireth",
-        "com.activision.callofduty.shooter"
+        PKG_QQ_SPEED, PKG_SPEED_DRIFTERS,
+        "com.miHoYo.GenshinImpact", "com.mobile.legends",
+        "com.dts.freefireth", "com.activision.callofduty.shooter"
     );
 
-    // Refresh Rate Targets
-    public static final int REFRESH_RATE_DEFAULT = 60;
-    public static final int REFRESH_RATE_90 = 90;
-    public static final int REFRESH_RATE_120 = 120;
-
-    // Game-Specific Optimizations
     public static GameInfo getGameInfo(String packageName) {
         if (PKG_QQ_SPEED.equals(packageName)) {
-            return new GameInfo(
-                NAME_QQ_SPEED,
-                PKG_QQ_SPEED,
-                VER_QQ_SPEED,
-                true,  // Anti-Bypass Loading
-                true,  // iOS-like Smooth
-                REFRESH_RATE_120,
-                true,  // Force High Refresh
-                true   // Graphic Optimization
-            );
+            return new GameInfo(NAME_QQ_SPEED, PKG_QQ_SPEED, true, true, 120, true, true);
         } else if (PKG_SPEED_DRIFTERS.equals(packageName)) {
-            return new GameInfo(
-                NAME_SPEED_DRIFTERS,
-                PKG_SPEED_DRIFTERS,
-                VER_SPEED_DRIFTERS,
-                true,  // Anti-Bypass Loading
-                true,  // iOS-like Smooth
-                REFRESH_RATE_120,
-                true,
-                true
-            );
+            return new GameInfo(NAME_SPEED_DRIFTERS, PKG_SPEED_DRIFTERS, true, true, 120, true, true);
         }
         return null;
     }
@@ -65,22 +29,15 @@ public class GameConfig {
     }
 
     public static class GameInfo {
-        public String name;
-        public String packageName;
-        public String version;
-        public boolean antiBypassLoading;
-        public boolean iosLikeSmooth;
+        public String name, packageName;
+        public boolean antiBypassLoading, iosLikeSmooth, forceRefreshRate, optimizeGraphics;
         public int targetRefreshRate;
-        public boolean forceRefreshRate;
-        public boolean optimizeGraphics;
 
-        public GameInfo(String name, String packageName, String version,
-                        boolean antiBypassLoading, boolean iosLikeSmooth,
-                        int targetRefreshRate, boolean forceRefreshRate,
+        public GameInfo(String name, String packageName, boolean antiBypassLoading,
+                        boolean iosLikeSmooth, int targetRefreshRate, boolean forceRefreshRate,
                         boolean optimizeGraphics) {
             this.name = name;
             this.packageName = packageName;
-            this.version = version;
             this.antiBypassLoading = antiBypassLoading;
             this.iosLikeSmooth = iosLikeSmooth;
             this.targetRefreshRate = targetRefreshRate;
