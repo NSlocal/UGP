9937b9098e7a880bce364d05324f1e7a3ddeb5c4
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-25 09:03:26 +0700

    Update GameConfig.java

--- app/src/main/java/com/universal/performance/GameConfig.java
+++ app/src/main/java/com/universal/performance/GameConfig.java
@@ -6,14 +6,18 @@ import java.util.List;
 public class GameConfig {
     public static final String PKG_QQ_SPEED = "com.tencent.tmgp.speedmobile";
     public static final String PKG_SPEED_DRIFTERS = "com.garena.game.fctw";
+    
     public static final List<String> SUPPORTED_GAMES = Arrays.asList(
-        PKG_QQ_SPEED, PKG_SPEED_DRIFTERS, "com.miHoYo.GenshinImpact",
-        "com.mobile.legends", "com.dts.freefireth", "com.activision.callofduty.shooter"
+        PKG_QQ_SPEED, PKG_SPEED_DRIFTERS,
+        "com.mobile.legends", "com.dts.freefireth", 
+        "com.activision.callofduty.shooter", "com.miHoYo.GenshinImpact"
     );
 
     public static GameInfo getGameInfo(String pkg) {
-        if (PKG_QQ_SPEED.equals(pkg) || PKG_SPEED_DRIFTERS.equals(pkg)) {
-            return new GameInfo(true, true, 120, true, true);
+        if (PKG_QQ_SPEED.equals(pkg)) {
+            return new GameInfo(true, true, 120, true, true, "QQ Speed v1.58.0");
+        } else if (PKG_SPEED_DRIFTERS.equals(pkg)) {
+            return new GameInfo(true, true, 120, true, true, "Speed Drifters v1.54.0");
         }
         return null;
     }
@@ -25,8 +29,11 @@ public class GameConfig {
     public static class GameInfo {
         public boolean antiBypass, iosSmooth, forceRefresh, optimizeGfx;
         public int targetRate;
-        public GameInfo(boolean a, boolean b, int c, boolean d, boolean e) {
-            antiBypass = a; iosSmooth = b; targetRate = c; forceRefresh = d; optimizeGfx = e;
+        public String name;
+        
+        public GameInfo(boolean a, boolean b, int c, boolean d, boolean e, String n) {
+            antiBypass = a; iosSmooth = b; targetRate = c; 
+            forceRefresh = d; optimizeGfx = e; name = n;
         }
     }
 }
