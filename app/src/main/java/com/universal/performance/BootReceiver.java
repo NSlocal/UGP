e197d56a8ff9aa7ae3683f9bda2abf0386c8c3b0
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-25 11:23:55 +0700

    Update BootReceiver.java

--- app/src/main/java/com/universal/performance/BootReceiver.java
+++ app/src/main/java/com/universal/performance/BootReceiver.java
@@ -7,17 +7,13 @@ import android.content.SharedPreferences;
 import android.os.Build;
 
 public class BootReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context c, Intent i) {
+    @Override public void onReceive(Context c, Intent i) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(i.getAction())) {
             SharedPreferences p = c.getSharedPreferences("Prefs", Context.MODE_PRIVATE);
-            if (p.getBoolean("perf_service", false)) {
+            if (p.getBoolean("service", false)) {
                 Intent si = new Intent(c, PerformanceService.class);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    c.startForegroundService(si);
-                } else {
-                    c.startService(si);
-                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) c.startForegroundService(si);
+                else c.startService(si);
             }
         }
     }
