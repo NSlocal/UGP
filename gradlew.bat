a14a0bc427b0a168b1811f0519c85614b01b823b
NSlocal <nadyaillustrationartist@gmail.com>
2026-08-26 09:52:36 +0700

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,32 +1,3 @@
-@if "%DEBUG%"=="" @echo off
-setlocal
-set DIRNAME=%~dp0
-if "%DIRNAME%"=="" set DIRNAME=.
-set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
-
-if defined JAVA_HOME goto findJavaFromJavaHome
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1 && goto execute
-echo ERROR: JAVA_HOME not set and no java in PATH
-goto fail
-
-:findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
-if exist "%JAVA_EXE%" goto execute
-echo ERROR: JAVA_HOME invalid: %JAVA_HOME%
-goto fail
-
-:execute
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% ^
-  "-Dorg.gradle.appname=%APP_BASE_NAME%" ^
-  -classpath "%CLASSPATH%" ^
-  org.gradle.wrapper.GradleWrapperMain %*
-if %ERRORLEVEL% equ 0 goto end
-:fail
-exit /b 1
-:end
-endlocal
+@echo off
+set CLASSPATH=%~dp0gradle\wrapper\gradle-wrapper.jar
+java -Xmx64m -Xms64m -Dorg.gradle.appname=gradlew -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
